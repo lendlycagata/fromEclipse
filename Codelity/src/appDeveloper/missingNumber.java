c233add3e991ce06c04d5d46807eb0e93e24b103
Lendly S Cagata <38574713+lendlycagata@users.noreply.github.com>
2019-07-14 16:54:48 +0800

    7-14-19

--- Codelity/src/appDeveloper/missingNumber.java
+++ Codelity/src/appDeveloper/missingNumber.java
@@ -1,27 +1,21 @@
 package appDeveloper;
-
-import java.util.Arrays;
+import java.util.HashSet;
 
 class missingNumber{
-	
-	static int missing (int A[]){
+	  static int solution(int[] A) {
+      HashSet<Integer> seen = new HashSet<Integer>();
+      int min = 1;
+      for (int i=0;i<A.length;i++) {
+      if (A[i] > 0) seen.add(A[i]);
+         }
+         for(int i = 1 ; i < Integer.MAX_VALUE; i++) {
+         if(!seen.contains(i)) return i;
+         }
+         return min;
+  }
+public static void main(String[] args) {
+	int A[]={1,1,2,2,3,5};
+	System.out.println(solution(A));
+}
 		
-		int y =0;
-		Arrays.sort(A);
-		
-		for(int i=0; i< A.length ; i++){
-			if( y != A[i]){
-				y++;
-		}
-		
-		
-	}
-		return y;
-		
-	}
-	
-	public static void main(String[] args) {
-		int A[] ={1,2,1,3,4,6};
-		System.out.println(missing(A)+ "= is the missing number");
-	}
 }
